363865b34f403ab443a333b7d62849c17018adb2
Finnerex <61033009+Finnerex@users.noreply.github.com>
2023-04-03 13:51:47 -0400

    Changes by Gavin

--- src/main/java/co/tantleffbeef/pluggytesty/BoltRodInteractListener.java
+++ src/main/java/co/tantleffbeef/pluggytesty/BoltRodInteractListener.java
@@ -1,8 +1,11 @@
 package co.tantleffbeef.pluggytesty;
 
 import org.bukkit.*;
+import org.bukkit.entity.Damageable;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -45,7 +48,7 @@ public class BoltRodInteractListener implements Listener {
 
     }
 
-    private boolean fireBolt(float range, Player player) { // returns if hit target
+    private Entity fireBolt(float range, Player player) {
 
         Location location = player.getEyeLocation();
 
@@ -56,7 +59,7 @@ public class BoltRodInteractListener implements Listener {
             player.spawnParticle(Particle.SPELL_INSTANT, location, 2);
         }
 
-        return false;
+        return entity;
     }
 }
 
