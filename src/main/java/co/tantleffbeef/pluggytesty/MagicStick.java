ba0275b29eba0e5d19057cf29c997039703e7a1a
Finnerex <61033009+Finnerex@users.noreply.github.com>
2023-04-02 23:05:23 -0400

    Changes by Gavin

--- src/main/java/co/tantleffbeef/pluggytesty/MagicStick.java
+++ src/main/java/co/tantleffbeef/pluggytesty/MagicStick.java
@@ -5,12 +5,12 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class MagicStick implements CommandExecutor {
 
