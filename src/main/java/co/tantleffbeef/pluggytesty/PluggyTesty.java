9025097eeae28689e739a2dba7a94a03cd3ae7b9
Finnerex <61033009+Finnerex@users.noreply.github.com>
2023-04-02 22:20:05 -0400

    OCaml!!!!11!

--- src/main/java/co/tantleffbeef/pluggytesty/PluggyTesty.java
+++ src/main/java/co/tantleffbeef/pluggytesty/PluggyTesty.java
@@ -7,16 +7,19 @@ public final class PluggyTesty extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("penis haha");
-        ;
-        ;
-        ;
-        ;
-        ;
-        ;
-        ;
-        ;
-        ;
+        let balls (a:int option) =
+        match a with
+        | Some x -> x
+        | None -> 0
 
+        Exception die
+
+        let jaws (b:int list result) =
+        | Error -> raise die
+        | Ok x ->
+            (match x with
+            | [] -> []
+            | x :: xs -> (jaws (Ok xs)) @ x
     }
 
     @Override
