044eefaa5d248338aec63a1b167820aa95c144ac
Finnerex <61033009+Finnerex@users.noreply.github.com>
2023-04-02 23:37:03 -0400

    balls

--- src/main/java/co/tantleffbeef/pluggytesty/MagicStick.java
+++ src/main/java/co/tantleffbeef/pluggytesty/MagicStick.java
@@ -43,4 +43,5 @@ public class MagicStick implements CommandExecutor {
 
         return true;
     }
+
 }
---
+++ src/main/java/co/tantleffbeef/pluggytesty/PlayerInteractEventListener.java
@@ -0,0 +1,50 @@
+package co.tantleffbeef.pluggytesty;
+
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Fireball;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.util.Vector;
+
+import java.util.Objects;
+
+public class PlayerInteractEventListener implements Listener {
+    private static final int FIREBALL_SPEED = 5;
+    private static final float FIREBALL_STRENGTH = 3.0f;
+
+    @EventHandler
+    private void onPlayerInteract(PlayerInteractEvent event) {
+        ItemStack leItem = event.getItem();
+
+        if (leItem == null || leItem.getType() != Material.STICK)
+            return;
+
+        ItemMeta leMeta = leItem.getItemMeta();
+        if (!leMeta.getLore().get(0).equals(MagicStick.STICK_LORE))
+            return;
+
+        Player player = event.getPlayer();
+
+        Location playerLocation = player.getEyeLocation();
+        Vector playerDirection = playerLocation.getDirection().normalize();
+        // get player direction
+
+        // summon a fireball in correct direction
+        World fireballWorld = Objects.requireNonNull(playerLocation.getWorld());
+        Fireball fireball = (Fireball) fireballWorld.spawnEntity(
+                playerLocation.add(playerDirection), EntityType.FIREBALL);
+
+        //change fireballyness
+        fireball.setYield(FIREBALL_STRENGTH);
+        fireball.setIsIncendiary(true);
+
+        fireball.setDirection(playerDirection.multiply(FIREBALL_SPEED));
+    }
+}
