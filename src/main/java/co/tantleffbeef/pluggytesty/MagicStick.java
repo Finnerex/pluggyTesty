c98d2d4eef888512a45617bec82b59caa95c82bf
Finnerex <61033009+Finnerex@users.noreply.github.com>
2023-04-02 22:45:33 -0400

    no more OCaml >:(

---
+++ src/main/java/co/tantleffbeef/pluggytesty/MagicStick.java
@@ -0,0 +1,33 @@
+package co.tantleffbeef.pluggytesty;
+
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.Arrays;
+
+public class MagicStick implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+
+        ItemStack stick = new ItemStack(Material.STICK);
+        // custom name, description, make it enchanted
+        stick.addEnchantment(Enchantment.RIPTIDE, 10);
+
+        ItemMeta meta = stick.getItemMeta();
+        // Change some stuff
+        meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        meta.setDisplayName("Magic Rod of Destiny!!!!!!111!");
+        meta.setLore(Arrays.stream(new String[] {"This stick has penetrated many villains over the years."}).toList());
+        stick.setItemMeta(meta);
+
+
+        return true;
+    }
+}
--- src/main/java/co/tantleffbeef/pluggytesty/PluggyTesty.java
+++ src/main/java/co/tantleffbeef/pluggytesty/PluggyTesty.java
@@ -7,19 +7,8 @@ public final class PluggyTesty extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("penis haha");
-        let balls (a:int option) =
-        match a with
-        | Some x -> x
-        | None -> 0
+        getCommand("givemewood").setExecutor(new MagicStick());
 
-        Exception die
-
-        let jaws (b:int list result) =
-        | Error -> raise die
-        | Ok x ->
-            (match x with
-            | [] -> []
-            | x :: xs -> (jaws (Ok xs)) @ x
     }
 
     @Override
