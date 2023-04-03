b6d7944013fd93346a6abce6415ed04cc0cbb4b8
Finnerex <61033009+Finnerex@users.noreply.github.com>
2023-04-03 12:47:37 -0400

    shooty shoot

--- src/main/java/co/tantleffbeef/pluggytesty/BoltRodInteractListener.java
+++ src/main/java/co/tantleffbeef/pluggytesty/BoltRodInteractListener.java
@@ -41,7 +41,7 @@ public class BoltRodInteractListener implements Listener {
         if (player.hasCooldown(Material.BLAZE_ROD))
             return;
 
-        fireBolt(50, player);
+        fireBolt(20, player);
 
     }
 
