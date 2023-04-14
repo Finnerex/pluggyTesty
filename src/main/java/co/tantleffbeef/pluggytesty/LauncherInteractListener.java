9469c5276a0dbce3069bdd367686bf05567a6add
Finnerex <61033009+Finnerex@users.noreply.github.com>
2023-04-14 17:53:59 -0400

    launchy Launch

--- src/main/java/co/tantleffbeef/pluggytesty/LauncherInteractListener.java
+++ src/main/java/co/tantleffbeef/pluggytesty/LauncherInteractListener.java
@@ -65,7 +65,7 @@ public class LauncherInteractListener implements Listener {
                 }
 
                 float p = location.getPitch();
-                location.setPitch(p + 5 + (Math.abs(p) / -18));
+                location.setPitch(p + 5 + (p / -18));
                 runs ++;
             }
         };
