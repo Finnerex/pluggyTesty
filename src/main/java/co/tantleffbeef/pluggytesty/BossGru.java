42ab7f5874ccdb6de5350320138f2f0e133d516e
Finnerex <61033009+Finnerex@users.noreply.github.com>
2023-04-21 13:34:21 -0400

    Changes by Noah Simcox

--- src/main/java/co/tantleffbeef/pluggytesty/BossGru.java
+++ src/main/java/co/tantleffbeef/pluggytesty/BossGru.java
@@ -1,22 +1,18 @@
 package co.tantleffbeef.pluggytesty;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
-import org.bukkit.entity.Villager;
-import org.bukkit.entity.Zombie;
+import org.bukkit.entity.*;
 import org.bukkit.inventory.EntityEquipment;
+import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class BossGru implements CommandExecutor {
 
@@ -42,18 +38,47 @@ public class BossGru implements CommandExecutor {
         gru.setHealth(500);
 
 
-//        BukkitRunnable runnable = new BukkitRunnable() {
-//            @Override
-//            public void run() {
-//
-//                location eye = gru.getEyeLocation();;
-//                if (gru.isDead())
-//                    gru.getWorld().spawnEntity(eye, EntityType.);
-//                    cancel();
-//                    return;
-//
-//            }
-//        }
+        BukkitRunnable runnable = new BukkitRunnable() {
+
+            double prevHealth = 500;
+            double currHealth = 500;
+            @Override
+            public void run() {
+
+                Location eye = gru.getEyeLocation();
+
+                if (gru.isDead()) { //dead command
+                    for (int i = 0; i < 15; i++) {
+                        ((Zombie) gru.getWorld().spawnEntity(eye, EntityType.ZOMBIE)).setAge(1);
+                    }
+                    gru.getWorld().playSound(eye, Sound.ENTITY_PLAYER_LEVELUP, 20, 0.1f);
+
+                    cancel();
+                    return;
+                }
+
+                currHealth = gru.getHealth();
+
+
+                if (currHealth < prevHealth){ //spawns zombie hoard on hit
+
+                    for (int i = 0; i < 5; i++){
+
+                        Zombie swordZombie = (Zombie) w.spawnEntity(eye, EntityType.ZOMBIE);
+                        EntityEquipment equipment = swordZombie.getEquipment();
+                        equipment.setItemInMainHand(new ItemStack(Material.IRON_SWORD));
+                        equipment.setHelmet(new ItemStack(Material.LEATHER_HELMET));
+                        equipment.setChestplate(new ItemStack(Material.LEATHER_CHESTPLATE));
+                        equipment.setLeggings(new ItemStack(Material.LEATHER_LEGGINGS));
+                        equipment.setBoots(new ItemStack(Material.LEATHER_BOOTS));
+                    }
+
+                    prevHealth = currHealth;
+                }
+
+            }
+        };
+        runnable.runTaskTimer(plugin, 0, 1);
 
         return true;
     }
