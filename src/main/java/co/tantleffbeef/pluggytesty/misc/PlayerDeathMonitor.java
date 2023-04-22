c6e1ec97bfec3fcff70525068a51bdc24ad54bf0
Finnerex <61033009+Finnerex@users.noreply.github.com>
2023-04-21 23:12:14 -0400

    now working on armor trims and shid

--- src/main/java/co/tantleffbeef/pluggytesty/misc/PlayerDeathMonitor.java
+++ src/main/java/co/tantleffbeef/pluggytesty/misc/PlayerDeathMonitor.java
@@ -2,6 +2,7 @@ package co.tantleffbeef.pluggytesty.misc;
 
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
@@ -11,7 +12,7 @@ import org.bukkit.inventory.PlayerInventory;
 import java.util.List;
 
 public class PlayerDeathMonitor implements Listener {
-    @EventHandler/*(priority = EventPriority.MONITOR)*/
+    @EventHandler(priority = EventPriority.MONITOR)
     public void onDeath(PlayerDeathEvent death) {
         death.setKeepLevel(true);
         death.setKeepInventory(true);
