a30178f814caf026457dd2396289bb97aa5f929e
Finnerex <61033009+Finnerex@users.noreply.github.com>
2023-04-21 23:21:17 -0400

    spcecial keep inventory

--- src/main/java/co/tantleffbeef/pluggytesty/misc/PlayerDeathMonitor.java
+++ src/main/java/co/tantleffbeef/pluggytesty/misc/PlayerDeathMonitor.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
@@ -13,14 +14,11 @@ import org.bukkit.inventory.PlayerInventory;
 import java.util.List;
 
 public class PlayerDeathMonitor implements Listener {
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void onDamage(EntityDamageEvent event) {
-        if (!(event.getEntity() instanceof Player player)) return;
+    @EventHandler/*(priority = EventPriority.MONITOR)*/
+    public void onDeath(PlayerDeathEvent event) {
 
-        if (event.getFinalDamage() < player.getHealthScale())
-            return;
-
-        ItemStack[] drops = player.getInventory().getExtraContents();
+        Player victim = event.getEntity();
+        ItemStack[] drops = victim.getInventory().getContents();
         int c = 0;
 
         Bukkit.broadcastMessage("Location: " + player.getLocation());
@@ -31,7 +29,22 @@ public class PlayerDeathMonitor implements Listener {
             // If it isn't, plonk it on the floor
             Bukkit.broadcastMessage("drop #" + c + ": " + d);
         }
-
-
     }
+//    public void onDamage(EntityDamageEvent event) {
+//        if (!(event.getEntity() instanceof Player player)) return;
+//
+//        if (event.getFinalDamage() < player.getHealthScale())
+//            return;
+//
+//        ItemStack[] drops = player.getInventory().getContents();
+//        int c = 0;
+//
+//        Bukkit.broadcastMessage("Location: " + player.getLocation());
+//
+//        for(ItemStack d : drops) {
+//            c++;
+//            // check if item is what we want to keep.
+//            // If it isn't, plonk it on the floor
+//            Bukkit.broadcastMessage("drop #" + c + ": " + d);
+//        }
 }
