c51736a1d682f64e5ff31abc6dc3a45a77c57608
Finnerex <61033009+Finnerex@users.noreply.github.com>
2023-04-22 15:05:04 -0400

    spcecial keep inventory

--- src/main/java/co/tantleffbeef/pluggytesty/misc/PlayerDeathMonitor.java
+++ src/main/java/co/tantleffbeef/pluggytesty/misc/PlayerDeathMonitor.java
@@ -9,6 +9,7 @@ import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.Plugin;
@@ -22,46 +23,48 @@ public class PlayerDeathMonitor implements Listener {
     public PlayerDeathMonitor(Plugin plugin) { this.plugin = plugin; }
     @EventHandler/*(priority = EventPriority.MONITOR)*/
     public void onDeath(PlayerDeathEvent event) {
+        event.setKeepInventory(true);
 
         Player victim = event.getEntity();
         World w = victim.getWorld();
         ItemStack[] drops = victim.getInventory().getContents();
-        int c = 0;
-
-        Bukkit.broadcastMessage("Location: " + victim.getLocation());
+//        int c = 0;
+//
+//        Bukkit.broadcastMessage("Location: " + victim.getLocation());
 
         for(int i = 9; i < 36; i++) {
             if(drops[i].getMaxStackSize() != 1) {
                 w.dropItemNaturally(victim.getLocation(), drops[i]);
+//                drops[i] = null;
+            }
+
+        }
+
+//        plugin.getServer().getScheduler().runTask(plugin, () -> {
+//            victim.getInventory().setContents(drops);
+//            victim.updateInventory();
+//        });
+
+    }
+
+    @EventHandler
+    public void onRespawn(PlayerRespawnEvent event) {
+        Player player = event.getPlayer();
+
+        ItemStack[] drops = player.getInventory().getContents();
+
+        for(int i = 9; i < 36; i++) {
+            if(drops[i].getMaxStackSize() != 1) {
                 drops[i] = null;
             }
 
         }
 
-
-        plugin.getServer().getScheduler().runTask(plugin, () -> {
-            victim.getInventory().setContents(drops);
-            victim.updateInventory();
-        });
-
+        player.getInventory().setContents(drops);
+        player.updateInventory();
 
     }
 
-//    public void onDamage(EntityDamageEvent event) {
-//        if (!(event.getEntity() instanceof Player player)) return;
-//
-//        if (event.getFinalDamage() < player.getHealthScale())
-//            return;
-//
-//        ItemStack[] drops = player.getInventory().getContents();
-//        int c = 0;
-//
-//        Bukkit.broadcastMessage("Location: " + player.getLocation());
-//
-//        for(ItemStack d : drops) {
-//            c++;
-//            // check if item is what we want to keep.
-//            // If it isn't, plonk it on the floor
-//            Bukkit.broadcastMessage("drop #" + c + ": " + d);
-//        }
+
+
 }
