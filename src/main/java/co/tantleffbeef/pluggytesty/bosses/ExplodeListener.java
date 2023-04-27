d46551eacc2f02244b8f165c9ad89e260085d221
Finnerex <61033009+Finnerex@users.noreply.github.com>
2023-04-27 18:04:32 -0400

    Special Surprise ;) and replaced

--- src/main/java/co/tantleffbeef/pluggytesty/bosses/ExplodeListener.java
+++ src/main/java/co/tantleffbeef/pluggytesty/bosses/ExplodeListener.java
@@ -1,6 +1,8 @@
 package co.tantleffbeef.pluggytesty.bosses;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Creeper;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityExplodeEvent;
@@ -9,7 +11,11 @@ public class ExplodeListener implements Listener {
 
     @EventHandler
     public void onExplode(EntityExplodeEvent event) {
-        Bukkit.broadcastMessage("Entity: " + event.getEntity());
+        if (!(event.getEntity() instanceof Creeper creeper)) return;
+
+        if (!creeper.getName().equals(ChatColor.GREEN + "BOUNCER")) return;
+
+        event.setCancelled(true);
     }
 
 }
