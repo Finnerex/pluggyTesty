fddc21e5a9af2e5acbc171ba2ddc72a8515f2538
Finnerex <61033009+Finnerex@users.noreply.github.com>
2023-04-27 16:12:51 -0400

    Special Surprise ;)

---
+++ src/main/java/co/tantleffbeef/pluggytesty/bosses/BossFireWorker.java
@@ -0,0 +1,161 @@
+package co.tantleffbeef.pluggytesty.bosses;
+
+import org.bukkit.*;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Creeper;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Firework;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.meta.FireworkMeta;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.util.Vector;
+import org.joml.Random;
+
+public class BossFireWorker implements CommandExecutor {
+    private final Plugin plugin;
+
+    public FireWorker(Plugin plugin) { this.plugin = plugin; }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (!(sender instanceof Player player))
+            return false;
+
+        World w = player.getWorld();
+        Location l = player.getEyeLocation();
+
+        Creeper bouncer = (Creeper) w.spawnEntity(l, EntityType.CREEPER);
+        bouncer.setPowered(true);
+
+        bouncer.getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(0.5);
+        bouncer.getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(1000);
+        bouncer.setHealth(1000);
+
+        bouncer.setCustomName(ChatColor.GREEN + "BOUNCER");
+
+        BukkitRunnable runnable = new BukkitRunnable() {
+
+            @Override
+            public void run() {
+
+                if (bouncer.isDead()) {
+                    cancel();
+                    return;
+                }
+
+                int jump = new Random().nextInt(3);
+                bouncer.setVelocity(bouncer.getVelocity().add(new Vector(0, jump, 0)));
+
+                int attack = new Random().nextInt(10);
+
+                if (attack == 0)
+                    workFires(bouncer);
+                if (attack == 1)
+                    spreadWorks(bouncer);
+//                if (attack == 2)
+//                    longWorks(bouncer);
+
+            }
+        };
+
+        runnable.runTaskTimer(plugin, 0, 10);
+
+        return true;
+    }
+
+    private void workFires(Creeper bouncer) {
+
+        BukkitRunnable runnable = new BukkitRunnable() {
+
+            int runs = 0;
+
+            @Override
+            public void run() {
+
+                if (runs > 9) {
+                    cancel();
+                    return;
+                }
+
+                Location loc = bouncer.getEyeLocation();
+
+                Firework f = (Firework) bouncer.getWorld().spawnEntity(loc, EntityType.FIREWORK);
+                f.setShotAtAngle(true);
+                f.getLocation().setDirection(loc.getDirection());
+                f.setBounce(true);
+                f.setShooter(bouncer);
+
+                FireworkMeta meta = f.getFireworkMeta();
+
+                meta.addEffect(buildFirework());
+                meta.setPower(4);
+
+                f.setFireworkMeta(meta);
+
+                runs ++;
+
+            }
+        };
+
+        runnable.runTaskTimer(plugin, 0, 5);
+
+    }
+
+    private FireworkEffect buildFirework() {
+        Random r = new Random();
+        FireworkEffect.Builder builder = FireworkEffect.builder();
+
+        builder.flicker(r.nextInt(2) == 1);
+        builder.trail(r.nextInt(2) == 1);
+        builder.withColor(Color.fromRGB(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
+        builder.withFade(Color.fromRGB(r.nextInt(256), r.nextInt(256), r.nextInt(256)));
+        builder.with(FireworkEffect.Type.values()[r.nextInt(5)]);
+
+        return builder.build();
+    }
+
+    private void spreadWorks(Creeper bouncer) {
+
+        BukkitRunnable runnable = new BukkitRunnable() {
+
+            int runs = 0;
+            Location l = bouncer.getEyeLocation();
+
+            @Override
+            public void run() {
+
+                if (runs > 6) {
+                    cancel();
+                    return;
+                }
+
+                Firework f = (Firework) bouncer.getWorld().spawnEntity(l, EntityType.FIREWORK);
+                f.setShotAtAngle(true);
+                f.getLocation().setDirection(l.getDirection());
+                f.setBounce(true);
+                f.setShooter(bouncer);
+
+                FireworkMeta meta = f.getFireworkMeta();
+
+                meta.addEffect(buildFirework());
+                meta.setPower(4);
+
+                f.setFireworkMeta(meta);
+
+                l.setYaw(l.getYaw() + 45);
+
+                runs++;
+
+            }
+
+        };
+
+        runnable.runTaskTimer(plugin, 0, 5);
+
+    }
+}
