c7f9a1a492a6adbc83fc008437137bb00a31739a
Swusan <bmoregarfield@gmail.com>
2023-04-27 21:24:01 -0400

    ClusterBomb bug test

--- src/main/java/co/tantleffbeef/pluggytesty/weapons/ClusterBombInteractListener.java
+++ src/main/java/co/tantleffbeef/pluggytesty/weapons/ClusterBombInteractListener.java
@@ -1,6 +1,7 @@
 package co.tantleffbeef.pluggytesty.weapons;
 
 import co.tantleffbeef.pluggytesty.PluggyTesty;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Sound;
@@ -23,26 +24,27 @@ public class ClusterBombInteractListener implements Listener {
     }
     @EventHandler
     private void onPlayerInteract(PlayerInteractEvent event) {
+        Bukkit.broadcastMessage("event");
         if (event.getAction() != Action.RIGHT_CLICK_AIR &&
                 event.getAction() != Action.RIGHT_CLICK_BLOCK)
             return;
 
         ItemStack item = event.getItem();
-
+        Bukkit.broadcastMessage("Right-Click");
         if (item == null || item.getType() != Material.COAL_BLOCK)
             return;
 
         ItemMeta meta = item.getItemMeta();
-
+        Bukkit.broadcastMessage("Coal Block");
         if (meta == null || meta.getLore() == null || !meta.getLore().get(0).equals(ClusterBomb.CB_LORE))
             return;
-
+        Bukkit.broadcastMessage("Meta");
         event.setCancelled(true);
         Player player = event.getPlayer();
-
+        Bukkit.broadcastMessage("setCancelled");
         if (player.hasCooldown(Material.COAL_BLOCK))
             return;
-
+        Bukkit.broadcastMessage("Cooldown");
         player.playSound(player, Sound.ENTITY_ENDER_PEARL_THROW, 1, 1);
 
         player.setCooldown(Material.COAL_BLOCK, 10);
@@ -53,7 +55,8 @@ public class ClusterBombInteractListener implements Listener {
         pearl.setGravity(false);
         pearl.setVelocity(location.getDirection().normalize());
 
-        plugin.getServer().getScheduler().runTaskLater(plugin, () -> explode(pearl), 80);
+        Bukkit.broadcastMessage("RTL");
+        plugin.getServer().getScheduler().runTaskLater(plugin, () -> explode(pearl), 40);
 
     }
     private void explode(Snowball pearl) {
