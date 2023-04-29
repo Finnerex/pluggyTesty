76587e0dcb2661f60fad31d8a6226283d5363e62
Finnerex <61033009+Finnerex@users.noreply.github.com>
2023-04-29 15:20:47 -0400

    7/32 imports used!11!

--- src/main/java/co/tantleffbeef/pluggytesty/weapons/RandomEffectBowInteractListener.java
+++ src/main/java/co/tantleffbeef/pluggytesty/weapons/RandomEffectBowInteractListener.java
@@ -1,39 +1,13 @@
 package co.tantleffbeef.pluggytesty.weapons;
 
-import org.bukkit.*;
-import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
-import org.bukkit.block.PistonMoveReaction;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.EntityEvent;
 import org.bukkit.event.entity.EntityShootBowEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerTeleportEvent;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.metadata.MetadataValue;
-import org.bukkit.permissions.Permission;
-import org.bukkit.permissions.PermissionAttachment;
-import org.bukkit.permissions.PermissionAttachmentInfo;
-import org.bukkit.persistence.PersistentDataContainer;
-import org.bukkit.plugin.Plugin;
-import org.bukkit.potion.PotionData;
-import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
-import org.bukkit.projectiles.ProjectileSource;
-import org.bukkit.util.BoundingBox;
-import org.bukkit.util.Vector;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.List;
 import java.util.Random;
-import java.util.Set;
-import java.util.UUID;
 
 public class RandomEffectBowInteractListener implements Listener {
 
