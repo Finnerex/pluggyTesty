95e3f9c7c76bf08e36bffff894c7df37e2b1f92e
Noah Simcox <nssimcox@gmail.com>
2023-04-28 22:28:02 -0400

    change world ender to Digga

--- src/main/java/co/tantleffbeef/pluggytesty/custom/item/armor/CraftListener.java
+++ src/main/java/co/tantleffbeef/pluggytesty/custom/item/armor/CraftListener.java
@@ -15,7 +15,7 @@ public class CraftListener implements Listener {
     NamespacedKey key = NamespacedKey.minecraft("leather_helmet");
 
     @EventHandler
-    public void onCraft(CraftItemEvent event) {
+    public void onCraft(PrepareItemCraftEvent event) {
         Bukkit.broadcastMessage("Craft event" + getRecipe(key).equals(event.getRecipe()));
 
 //        if (event.getRecipe().equals(getRecipe(key))) {
