93a533e0ef83093132e1821b0657013ceec1f83a
Finnerex <61033009+Finnerex@users.noreply.github.com>
2023-04-29 16:02:39 -0400

    fixed EntityEffectListener

--- src/main/java/co/tantleffbeef/pluggytesty/armor/effect_listeners/EntityEffectListener.java
+++ src/main/java/co/tantleffbeef/pluggytesty/armor/effect_listeners/EntityEffectListener.java
@@ -2,14 +2,11 @@ package co.tantleffbeef.pluggytesty.armor.effect_listeners;
 
 import co.tantleffbeef.pluggytesty.armor.ArmorEffectType;
 import co.tantleffbeef.pluggytesty.armor.ArmorEquipListener;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPotionEffectEvent;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
-import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 public class EntityEffectListener implements Listener {
@@ -20,19 +17,13 @@ public class EntityEffectListener implements Listener {
 
     @EventHandler
     public void onEffect(EntityPotionEffectEvent event) {
-        Bukkit.broadcastMessage("effect event");
         if (!(event.getEntity() instanceof Player player))
             return;
-
-        Bukkit.broadcastMessage("is player");
 
         if (ArmorEquipListener.effectMap.get(player.getUniqueId()) != ArmorEffectType.NIGHT_VISION)
             return;
 
         PotionEffectType effect = event.getNewEffect().getType();
-        Bukkit.broadcastMessage("has effect");
-        Bukkit.broadcastMessage("blind " + (effect.equals(PotionEffectType.BLINDNESS)) + player.hasPotionEffect(PotionEffectType.BLINDNESS));
-        Bukkit.broadcastMessage("dark " + (effect.equals(PotionEffectType.DARKNESS)) + player.hasPotionEffect(PotionEffectType.DARKNESS));
 
         if (effect.equals(PotionEffectType.BLINDNESS))
             plugin.getServer().getScheduler().runTask(plugin, () -> player.removePotionEffect(PotionEffectType.BLINDNESS));
