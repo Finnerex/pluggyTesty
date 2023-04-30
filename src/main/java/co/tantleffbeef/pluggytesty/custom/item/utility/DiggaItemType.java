19bd3977ee5947939bca963f183739a1f583ae3e
Noah Simcox <nssimcox@gmail.com>
2023-04-30 16:08:25 -0400

    added armor and other stiff penisses

---
+++ src/main/java/co/tantleffbeef/pluggytesty/custom/item/utility/DiggaItemType.java
@@ -0,0 +1,9 @@
+package co.tantleffbeef.pluggytesty.custom.item.utility;
+
+import co.tantleffbeef.mcplanes.custom.item.InteractableItemType;
+import co.tantleffbeef.mcplanes.custom.item.SimpleItemType;
+
+//public class DiggaItemType extends SimpleItemType implements InteractableItemType {
+//
+//
+//}
