eba450470ef1c6b09f5819115880b515e1dbfbd4
Finnerex <61033009+Finnerex@users.noreply.github.com>
2023-04-30 10:20:20 -0400

    I got the class

---
+++ src/main/java/co/tantleffbeef/pluggytesty/custom/item/weapons/BoltRodItemType.java
@@ -0,0 +1,17 @@
+package co.tantleffbeef.pluggytesty.custom.item.weapons;
+
+import co.tantleffbeef.mcplanes.custom.item.InteractableItemType;
+import co.tantleffbeef.mcplanes.custom.item.SimpleItemType;
+import org.bukkit.Material;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.Plugin;
+
+public class BoltRodItemType extends SimpleItemType implements InteractableItemType {
+
+    public BoltRodItemType(Plugin namespace, String id, boolean customModel, String name) {
+        super(namespace, id, customModel, name, Material.BLAZE_ROD);
+        getClass().getClass().getClass().getClass().getClass().getClass().getClass().getClass().getClass().getClass().getClass().getClass().getClass().getClass().getClass().getClass().getClass().getClasses();
+    }
+
+
+}
