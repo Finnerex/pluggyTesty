9f05b39b016796675710269d7cb0954a755018bd
Swusan <bmoregarfield@gmail.com>
2023-04-30 12:42:36 -0400

    ClusterBomb changed into customItem

--- src/main/java/co/tantleffbeef/pluggytesty/PluggyTesty.java
+++ src/main/java/co/tantleffbeef/pluggytesty/PluggyTesty.java
@@ -11,6 +11,7 @@ import co.tantleffbeef.pluggytesty.armor.effect_listeners.*;
 import co.tantleffbeef.pluggytesty.bosses.*;
 import co.tantleffbeef.pluggytesty.custom.item.armor.SmithListener;
 import co.tantleffbeef.pluggytesty.custom.item.weapons.BoltRodItemType;
+import co.tantleffbeef.pluggytesty.custom.item.weapons.ClusterBombItemType;
 import co.tantleffbeef.pluggytesty.custom.item.weapons.MagicStickItemType;
 import co.tantleffbeef.pluggytesty.custom.item.armor.CraftListener;
 import co.tantleffbeef.pluggytesty.custom.item.weapons.AxeOfYourMotherItemType;
@@ -88,7 +89,6 @@ public final class PluggyTesty extends JavaPlugin {
         getCommand("giveheavyarmor").setExecutor(new HeavyArmor());
         getCommand("summongru").setExecutor(new BossGru(this));
         getCommand("summonbouncer").setExecutor(new BossFireWorker(this));
-        getCommand("givemeClusterBomb").setExecutor(new ClusterBomb());
         getCommand("givemedash").setExecutor(new Dash());
         getCommand("givemeBow").setExecutor(new RandomEffectBow());
 
@@ -101,7 +101,6 @@ public final class PluggyTesty extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new SwordsmansDreamInteractListener(this), this);
         getServer().getPluginManager().registerEvents(new GoerInteractListener(), this);
         getServer().getPluginManager().registerEvents(new LauncherInteractListener(this), this);
-        getServer().getPluginManager().registerEvents(new ClusterBombInteractListener(this), this);
         getServer().getPluginManager().registerEvents(new RandomEffectBowInteractListener(), this);
 
         getServer().getPluginManager().registerEvents(new ArmorEquipListener(this), this);
@@ -129,6 +128,7 @@ public final class PluggyTesty extends JavaPlugin {
                 ChatColor.AQUA + "Axe of Your Mother"));
         resourceManager.registerItem(new BoltRodItemType(this, "bolt_rod", false,
                 "Bolt Rod"));
+        resourceManager.registerItem(new ClusterBombItemType(this, "cluster_bomb", false, "Cluster Bomb"));
     }
 
 
