0b2de7f2f8cf59d279a077922e8f58fb643463a2
Finnerex <61033009+Finnerex@users.noreply.github.com>
2023-05-03 19:09:27 -0400

    Minotaurs Axe

--- src/main/java/co/tantleffbeef/pluggytesty/custom/item/weapons/MinotaursAxeItemType.java
+++ src/main/java/co/tantleffbeef/pluggytesty/custom/item/weapons/MinotaursAxeItemType.java
@@ -37,11 +37,11 @@ public class MinotaursAxeItemType extends SimpleItemType implements Interactable
 
         final ItemDisplay axe = player.getWorld().spawn(l, ItemDisplay.class, (display) -> {
             display.setItemStack(new ItemStack(Material.GOLDEN_AXE));
-            //display.setRotation(l.getDirection(l.getYaw() + 90));
+            display.getLocation().setDirection(l.getDirection());
         });
 
         final Vector direction = l.getDirection();
-        final Vector perpendicular = direction.clone().rotateAroundY(90);
+//        final Vector perpendicular = direction.clone().rotateAroundY(90);
 
         BukkitRunnable runnable = new BukkitRunnable() {
             int distance = 0;
@@ -56,7 +56,7 @@ public class MinotaursAxeItemType extends SimpleItemType implements Interactable
                 Location location = axe.getLocation();
 
                 axe.teleport(location.add(direction));
-                axe.getLocation().getDirection().rotateAroundAxis(perpendicular, 10);
+//                axe.getLocation().getDirection().rotateAroundAxis(perpendicular, 10);
 
                 distance ++;
             }
