bade30a95a9bd727afadd07cdc9f5f6bdb81fdb1
Finnerex <61033009+Finnerex@users.noreply.github.com>
2023-05-13 11:46:40 -0400

    no idear but im trying loottables

--- src/main/java/co/tantleffbeef/pluggytesty/expeditions/loot/LootTableTestCommand.java
+++ src/main/java/co/tantleffbeef/pluggytesty/expeditions/loot/LootTableTestCommand.java
@@ -23,7 +23,8 @@ public class LootTableTestCommand implements CommandExecutor {
 
 
         // make the loot table (for real later)
-        LootTable lootTable = Bukkit.getLootTable(new NamespacedKey("pluggy_testy", "loot_tables/chests/chest_test"));
+//        LootTable lootTable = Bukkit.getLootTable(new NamespacedKey("pluggy_testy", "loot_tables/chests/chest_test"));
+        LootTable lootTable = Bukkit.getLootTable(NamespacedKey.minecraft("chests/village/village_armorer"));
 
         if (lootTable == null) {
             Bukkit.broadcastMessage("that thang was null");
