fd9d541441a6b8df2fcbabefa4d2e8784966ca6a
Noah Simcox <nssimcox@gmail.com>
2023-05-14 19:49:48 -0400

    Changes by Xander

--- src/main/java/co/tantleffbeef/pluggytesty/bosses/BossTrial1.java
+++ src/main/java/co/tantleffbeef/pluggytesty/bosses/BossTrial1.java
@@ -59,15 +59,15 @@ public class BossTrial1 implements CommandExecutor {
 
         Location bossLoc = boss.getLocation();
 
-        Chicken zombHorse = player.getWorld().spawn(new Location(player.getWorld(), bossLoc.getX() + 5, bossLoc.getY(), bossLoc.getZ()), Chicken.class, (horse) -> {
-            // horse.setTamed(true);
+        ZombieHorse zombHorse = player.getWorld().spawn(new Location(player.getWorld(), bossLoc.getX() + 5, bossLoc.getY(), bossLoc.getZ()), ZombieHorse.class, (horse) -> {
+            horse.setTamed(true);
         });
         Zombie zombRider = player.getWorld().spawn(new Location(player.getWorld(), bossLoc.getX() + 5, bossLoc.getY(), bossLoc.getZ()), Zombie.class, (zomb) ->{
             zomb.getEquipment().setHelmet(new ItemStack(Material.CARVED_PUMPKIN));
         });
 
-        Spider skeleHorse = player.getWorld().spawn(new Location(player.getWorld(), bossLoc.getX() - 5, bossLoc.getY(), bossLoc.getZ()), Spider.class, (horse) ->{
-            // horse.setTamed(true);
+        SkeletonHorse skeleHorse = player.getWorld().spawn(new Location(player.getWorld(), bossLoc.getX() - 5, bossLoc.getY(), bossLoc.getZ()), SkeletonHorse.class, (horse) ->{
+            horse.setTamed(true);
         });
         Skeleton skeleRider = player.getWorld().spawn(new Location(player.getWorld(), bossLoc.getX() - 5, bossLoc.getY(), bossLoc.getZ()), Skeleton.class, (skele) ->{
             skele.getEquipment().setHelmet(new ItemStack(Material.CARVED_PUMPKIN));
@@ -75,6 +75,8 @@ public class BossTrial1 implements CommandExecutor {
 
         zombHorse.addPassenger(zombRider);
         skeleHorse.addPassenger(skeleRider);
+        zombHorse.setTarget(player);
+        skeleHorse.setTarget(player);
 
         // attacks
         BukkitRunnable runnable = new BukkitRunnable() {
