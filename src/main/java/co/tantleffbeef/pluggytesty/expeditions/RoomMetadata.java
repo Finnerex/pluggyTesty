2596aec06efc6ebab2be9d0365c67841046b959e
gIsForGravity <gavin.f.tantleff@gmail.com>
2023-05-19 20:27:24 -0400

    make a room store its players and comment out that

--- src/main/java/co/tantleffbeef/pluggytesty/expeditions/RoomMetadata.java
+++ src/main/java/co/tantleffbeef/pluggytesty/expeditions/RoomMetadata.java
@@ -4,8 +4,8 @@ import co.tantleffbeef.pluggytesty.expeditions.rooms.Room;
 import org.bukkit.Location;
 
 public record RoomMetadata(Room room, Location minimum, Location maximum) { }
-im coding fr yep eating ts up yall could neva <3
+/*im coding fr yep eating ts up yall could neva <3
 I love you so much <33333
 tell ya lil bfs too back tf up
 YOU MINE
-hehe
+hehe*/
