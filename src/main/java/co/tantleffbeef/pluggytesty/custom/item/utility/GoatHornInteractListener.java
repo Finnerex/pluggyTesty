548d6d73c0046627b8082bed2219caa1fb1edcc2
Finnerex <61033009+Finnerex@users.noreply.github.com>
2023-05-21 21:04:10 -0400

    goat horn effects

--- src/main/java/co/tantleffbeef/pluggytesty/custom/item/utility/GoatHornInteractListener.java
+++ src/main/java/co/tantleffbeef/pluggytesty/custom/item/utility/GoatHornInteractListener.java
@@ -34,16 +34,16 @@ public class GoatHornInteractListener implements Listener {
         String instrumentString = instrument.getKey().getKey();
 
         player.addPotionEffect(switch (instrumentString.substring(0, instrumentString.indexOf("_goat_horn"))) {
-            case "ponder" -> PotionEffectType.JUMP.createEffect(EFFECT_DURATION_TICKS, 4);
-            case "admire" -> PotionEffectType.FAST_DIGGING.createEffect(EFFECT_DURATION_TICKS, 2);
-            case "seek" -> PotionEffectType.ABSORPTION.createEffect(EFFECT_DURATION_TICKS, 10);
-            case "call" -> PotionEffectType.SPEED.createEffect(EFFECT_DURATION_TICKS, 3);
-            case "dream" -> PotionEffectType.REGENERATION.createEffect(EFFECT_DURATION_TICKS, 3);
+            case "ponder" -> PotionEffectType.JUMP.createEffect(EFFECT_DURATION_TICKS, 3);
+            case "admire" -> PotionEffectType.FAST_DIGGING.createEffect(EFFECT_DURATION_TICKS, 1);
+            case "seek" -> PotionEffectType.ABSORPTION.createEffect(EFFECT_DURATION_TICKS, 4);
+            case "call" -> PotionEffectType.SPEED.createEffect(EFFECT_DURATION_TICKS, 2);
+            case "dream" -> PotionEffectType.REGENERATION.createEffect(EFFECT_DURATION_TICKS, 2);
             case "feel" -> PotionEffectType.INCREASE_DAMAGE.createEffect(EFFECT_DURATION_TICKS, 1);
             default -> PotionEffectType.LUCK.createEffect(0, 0);
         });
 
-        player.setCooldown(Material.GOAT_HORN, EFFECT_DURATION_TICKS * 2);
+        player.setCooldown(Material.GOAT_HORN, (int) (EFFECT_DURATION_TICKS * 1.4f));
 
     }
 }
