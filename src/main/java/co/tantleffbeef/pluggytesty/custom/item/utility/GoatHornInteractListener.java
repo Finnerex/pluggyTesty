eb7262ead6643f18e4e424060715b79216c98372
Finnerex <61033009+Finnerex@users.noreply.github.com>
2023-05-21 20:21:07 -0400

    goat horn effects

--- src/main/java/co/tantleffbeef/pluggytesty/custom/item/utility/GoatHornInteractListener.java
+++ src/main/java/co/tantleffbeef/pluggytesty/custom/item/utility/GoatHornInteractListener.java
@@ -1,7 +1,9 @@
 package co.tantleffbeef.pluggytesty.custom.item.utility;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Instrument;
 import org.bukkit.Material;
+import org.bukkit.MusicInstrument;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,6 +11,7 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.MusicInstrumentMeta;
 import org.bukkit.potion.PotionEffectType;
 
 import java.util.List;
@@ -23,33 +26,29 @@ public class GoatHornInteractListener implements Listener {
         Player player = event.getPlayer();
         ItemStack item = event.getItem();
 
-        if (item == null || item.getType() != Material.GOAT_HORN)
+        if (item == null || item.getType() != Material.GOAT_HORN || player.hasCooldown(Material.GOAT_HORN))
             return;
 
-        ItemMeta meta = item.getItemMeta();
+        MusicInstrumentMeta meta = ((MusicInstrumentMeta) item.getItemMeta());
         assert meta != null;
 
+        MusicInstrument instrument = meta.getInstrument();
+        assert instrument != null;
+
+        String metaString = instrument.toString();
 
 
+        player.addPotionEffect(switch (metaString.substring(0, metaString.indexOf("_goat_horn"))) {
+            case "ponder" -> PotionEffectType.JUMP.createEffect(EFFECT_DURATION_TICKS, 4);
+            case "admire" -> PotionEffectType.FAST_DIGGING.createEffect(EFFECT_DURATION_TICKS, 2);
+            case "seek" -> PotionEffectType.ABSORPTION.createEffect(EFFECT_DURATION_TICKS, 10);
+            case "call" -> PotionEffectType.SPEED.createEffect(EFFECT_DURATION_TICKS, 3);
+            case "dream" -> PotionEffectType.REGENERATION.createEffect(EFFECT_DURATION_TICKS, 3);
+            case "feel" -> PotionEffectType.INCREASE_DAMAGE.createEffect(EFFECT_DURATION_TICKS, 1);
+            default -> PotionEffectType.LUCK.createEffect(0, 0);
+        });
 
-
-//        List<String> lore = meta.getLore();
-//        assert lore != null;
-
-        Bukkit.broadcastMessage(item.toString());
-        Bukkit.broadcastMessage(meta.toString());
-
-//        player.addPotionEffect(switch (lore.get(0)) {
-//            case "Ponder" -> PotionEffectType.JUMP.createEffect(EFFECT_DURATION_TICKS, 4);
-//            case "Admire" -> PotionEffectType.FAST_DIGGING.createEffect(EFFECT_DURATION_TICKS, 2);
-//            case "Seek" -> PotionEffectType.ABSORPTION.createEffect(EFFECT_DURATION_TICKS, 10);
-//            case "Call" -> PotionEffectType.SPEED.createEffect(EFFECT_DURATION_TICKS, 3);
-//            case "Dream" -> PotionEffectType.REGENERATION.createEffect(EFFECT_DURATION_TICKS, 3);
-//            case "Feel" -> PotionEffectType.INCREASE_DAMAGE.createEffect(EFFECT_DURATION_TICKS, 1);
-//            default -> PotionEffectType.LUCK.createEffect(0, 0);
-//        });
-//
-//        player.setCooldown(Material.GOAT_HORN, EFFECT_DURATION_TICKS * 2);
+        player.setCooldown(Material.GOAT_HORN, EFFECT_DURATION_TICKS * 2);
 
     }
 }
