8bf871cdb52c7a2278e0403bdd85408bcf3cfa87
Finnerex <61033009+Finnerex@users.noreply.github.com>
2023-05-26 10:10:01 -0400

    Update ExpeditionLootTables.java

--- src/main/java/co/tantleffbeef/pluggytesty/expeditions/loot/ExpeditionLootTables.java
+++ src/main/java/co/tantleffbeef/pluggytesty/expeditions/loot/ExpeditionLootTables.java
@@ -3,6 +3,7 @@ package co.tantleffbeef.pluggytesty.expeditions.loot;
 public enum ExpeditionLootTables {
 
     TIER_1_LOW("chests/tier_1/low_rarity"),
+    TIER_1_MED("chests/tier_1/medium_rarity"),
 
     ;
 
