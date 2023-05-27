178d351794eab812bf8e22da3249bc0b675efbd1
Finnerex <61033009+Finnerex@users.noreply.github.com>
2023-05-27 11:49:30 -0400

    numbah_tweakin!

--- src/main/java/co/tantleffbeef/pluggytesty/custom/item/weapons/arrows/JestersArrowItemType.java
+++ src/main/java/co/tantleffbeef/pluggytesty/custom/item/weapons/arrows/JestersArrowItemType.java
@@ -2,6 +2,7 @@ package co.tantleffbeef.pluggytesty.custom.item.weapons.arrows;
 
 import co.tantleffbeef.mcplanes.custom.item.SimpleItemType;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.entity.Arrow;
@@ -9,6 +10,8 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.PotionMeta;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
 
 public class JestersArrowItemType extends SimpleItemType implements CustomArrow {
     public JestersArrowItemType(Plugin namespace, String id, boolean customModel, String name) {
@@ -18,14 +21,14 @@ public class JestersArrowItemType extends SimpleItemType implements CustomArrow 
     @Override
     public void modifyItemMeta(@NotNull ItemMeta meta) {
         super.modifyItemMeta(meta);
+        meta.setLore(List.of(ChatColor.DARK_GREEN + "High velocity, pierces up to 5 enemies"));
         ((PotionMeta) meta).setColor(Color.fromRGB(221, 187, 237));
     }
 
     @Override
     public void runCustomEffects(Arrow arrow) {
-        arrow.setVelocity(arrow.getVelocity().multiply(4));
-        arrow.setPierceLevel(10);
+        arrow.setVelocity(arrow.getVelocity().multiply(2.5f));
+        arrow.setPierceLevel(5);
         arrow.setKnockbackStrength(0);
-//        Bukkit.broadcastMessage("velocity: " + arrow.getVelocity() + "pierce: " + arrow.getPierceLevel() + "kb: " + arrow.getKnockbackStrength());
     }
 }
