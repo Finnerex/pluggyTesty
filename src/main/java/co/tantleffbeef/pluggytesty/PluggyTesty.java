df70aa5229cc37d9ccc31e64f376199a72417135
gIsForGravity <gavin.f.tantleff@gmail.com>
2023-06-03 16:46:33 -0400

    try to add filtering

--- src/main/java/co/tantleffbeef/pluggytesty/PluggyTesty.java
+++ src/main/java/co/tantleffbeef/pluggytesty/PluggyTesty.java
@@ -96,7 +96,7 @@ public final class PluggyTesty extends JavaPlugin {
 
         Debug.info("testing some shit man");
         try {
-            saveAllResourcesExcept(".class|acf-|META-INF");
+            saveAllResourcesExcept("\\b.*.class|\\b.*acf-|\\b.*META-INF");
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
