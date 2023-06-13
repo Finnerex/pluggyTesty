e2a1f428e90b0332e761563fc16c4cec642b43ce
Finnerex <61033009+Finnerex@users.noreply.github.com>
2023-06-12 21:27:34 -0400

    fix dash trim (double click)

--- src/main/java/co/tantleffbeef/pluggytesty/armor/effect_listeners/DashAbilityInteractListener.java
+++ src/main/java/co/tantleffbeef/pluggytesty/armor/effect_listeners/DashAbilityInteractListener.java
@@ -3,6 +3,7 @@ package co.tantleffbeef.pluggytesty.armor.effect_listeners;
 import co.tantleffbeef.pluggytesty.armor.ArmorEffectType;
 import co.tantleffbeef.pluggytesty.armor.ArmorEquipListener;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -25,6 +26,15 @@ public class DashAbilityInteractListener implements Listener {
 
         if (ArmorEquipListener.effectMap.get(player.getUniqueId()) != ArmorEffectType.DASH)
             return;
+
+        // use the boots to hold the double click timer
+        ItemStack boots = player.getInventory().getBoots();
+        assert boots != null;
+
+        if (!player.hasCooldown(boots.getType())) {
+            player.setCooldown(boots.getType(), 10);
+            return;
+        }
 
         ItemStack cp = player.getInventory().getChestplate();
         assert cp != null;
