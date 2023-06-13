6c1c9f278baac6e524faead7bc69da03b83b7276
Finnerex <61033009+Finnerex@users.noreply.github.com>
2023-06-13 17:17:46 -0400

    Dispenser????!?!?!?

--- src/main/java/co/tantleffbeef/pluggytesty/extra_listeners/SpecialArrowShootListener.java
+++ src/main/java/co/tantleffbeef/pluggytesty/extra_listeners/SpecialArrowShootListener.java
@@ -11,14 +11,17 @@ import org.bukkit.Bukkit;
 import org.bukkit.NamespacedKey;
 import org.bukkit.Sound;
 import org.bukkit.block.BlockFace;
+import org.bukkit.block.Dispenser;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockDispenseEvent;
 import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.player.PlayerPickupArrowEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.metadata.MetadataValue;
@@ -47,21 +50,14 @@ public class SpecialArrowShootListener implements Listener {
         if (!(event.getProjectile() instanceof Arrow arrow))
             return;
 
+        ItemStack item = event.getConsumable();
+        if (item == null)
+            return;
+
         // figure out if this arrow is a custom arrow
-        ItemMeta meta = event.getConsumable().getItemMeta();
-        if (meta == null)
-            return;
+        CustomArrow customArrow = checkArrow(item);
 
-        final var data = meta.getPersistentDataContainer();
-
-        if (!CustomItemNbt.hasCustomItemNbt(data, keyManager))
-            return;
-
-        final var itemNbt = CustomItemNbt.fromPersistentDataContainer(data, keyManager);
-        final var itemType = resourceManager.getCustomItemType(itemNbt.id);
-
-
-        if (!(itemType instanceof CustomArrow customArrow))
+        if (customArrow == null)
             return;
 
         // interfaces go hard!!!
@@ -70,6 +66,49 @@ public class SpecialArrowShootListener implements Listener {
         // so the arrow can be identified upon landing
         arrow.setMetadata("customArrowType", new FixedMetadataValue(plugin, customArrow));
 
+    }
+
+    @EventHandler // why the fuck doesn't it give you the item???
+    public void onDispense(BlockDispenseEvent event) {
+        if (!(event.getBlock() instanceof Dispenser dispenser))
+            return;
+
+        ItemStack item = event.getItem();
+
+        CustomArrow customArrow = checkArrow(item);
+
+        if (customArrow == null)
+            return;
+
+        event.setCancelled(true);
+
+        dispenser.getWorld().spawn(dispenser.getLocation().add(dispenser.getLocation().getDirection()), Arrow.class, (arrow) -> {
+           arrow.setVelocity(event.getVelocity());
+           customArrow.applySpawnEffects(arrow);
+           arrow.setMetadata("customArrowType", new FixedMetadataValue(plugin, customArrow));
+        });
+
+        item.setAmount(item.getAmount() - 1);
+    }
+
+    private CustomArrow checkArrow(ItemStack item) {
+        ItemMeta meta = item.getItemMeta();
+        if (meta == null)
+            return null;
+
+        final var data = meta.getPersistentDataContainer();
+
+        if (!CustomItemNbt.hasCustomItemNbt(data, keyManager))
+            return null;
+
+        final var itemNbt = CustomItemNbt.fromPersistentDataContainer(data, keyManager);
+        final var itemType = resourceManager.getCustomItemType(itemNbt.id);
+
+
+        if (!(itemType instanceof CustomArrow customArrow))
+            return null;
+
+        return customArrow;
     }
 
     @EventHandler
