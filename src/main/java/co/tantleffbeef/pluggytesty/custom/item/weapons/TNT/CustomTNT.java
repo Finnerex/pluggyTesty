1fb3a8cf3f0e8c61c392b1c61f703c151b91fffb
Noah Simcox <nssimcox@gmail.com>
2023-06-16 14:08:02 -0400

    added place event to cumstain tnt

--- src/main/java/co/tantleffbeef/pluggytesty/custom/item/weapons/TNT/CustomTNT.java
+++ src/main/java/co/tantleffbeef/pluggytesty/custom/item/weapons/TNT/CustomTNT.java
@@ -1,10 +1,11 @@
 package co.tantleffbeef.pluggytesty.custom.item.weapons.TNT;
 
+import co.tantleffbeef.mcplanes.custom.block.CustomBlockType;
 import co.tantleffbeef.mcplanes.custom.item.CustomItemType;
 import org.bukkit.block.Block;
 import org.bukkit.event.block.BlockPlaceEvent;
 
-public interface CustomTNT extends CustomItemType {
+public interface CustomTNT extends CustomBlockType {
 
 /**
  * Runs when the custom TNT explodes
