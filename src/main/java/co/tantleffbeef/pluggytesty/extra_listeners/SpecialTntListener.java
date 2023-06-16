755a9aa7d08051af851ff43f533b9a98b9d539dc
Noah Simcox <nssimcox@gmail.com>
2023-06-16 13:30:16 -0400

    bug

--- src/main/java/co/tantleffbeef/pluggytesty/extra_listeners/SpecialTntListener.java
+++ src/main/java/co/tantleffbeef/pluggytesty/extra_listeners/SpecialTntListener.java
@@ -14,16 +14,6 @@ import org.jetbrains.annotations.NotNull;
 
 public class SpecialTntListener implements Listener {
 
-    private final KeyManager<CustomNbtKey> keyManager;
-    private final ResourceManager resourceManager;
-    private final Plugin plugin;
-
-    public SpecialTntListener(@NotNull KeyManager<CustomNbtKey> keyManager, @NotNull ResourceManager resourceManager, @NotNull Plugin plugin) {
-        this.keyManager = keyManager;
-        this.resourceManager = resourceManager;
-        this.plugin = plugin;
-    }
-
     @EventHandler
     public void placeEvent(BlockPlaceEvent event){
 
