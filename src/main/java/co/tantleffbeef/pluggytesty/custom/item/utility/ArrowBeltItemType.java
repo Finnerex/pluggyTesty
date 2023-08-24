f54fadefa215a293bdc9f07fc0cc36ddff98bef0
Finnerex <61033009+Finnerex@users.noreply.github.com>
2023-08-24 14:07:42 -0400

    worky

--- src/main/java/co/tantleffbeef/pluggytesty/custom/item/utility/ArrowBeltItemType.java
+++ src/main/java/co/tantleffbeef/pluggytesty/custom/item/utility/ArrowBeltItemType.java
@@ -2,6 +2,7 @@ package co.tantleffbeef.pluggytesty.custom.item.utility;
 
 import co.tantleffbeef.mcplanes.custom.item.InteractableItemType;
 import co.tantleffbeef.mcplanes.custom.item.SimpleItemType;
+import co.tantleffbeef.pluggytesty.custom.item.weapons.arrows.CustomArrow;
 import co.tantleffbeef.pluggytesty.inventoryGUI.InventoryButton;
 import co.tantleffbeef.pluggytesty.inventoryGUI.InventoryGUI;
 import co.tantleffbeef.pluggytesty.inventoryGUI.InventorySelectorButton;
@@ -16,6 +17,8 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityShootBowEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.metadata.FixedMetadataValue;
+import org.bukkit.metadata.MetadataValue;
 import org.bukkit.plugin.Plugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -28,6 +31,7 @@ public class ArrowBeltItemType extends SimpleItemType implements InteractableIte
 
     private final Map<UUID, InventoryGUI> playerBelts;
     private final Map<UUID, Integer> playerLastShotPos;
+    private final Plugin plugin;
 
     private final InventoryGUI DEFAULT_BELT;
 
@@ -36,6 +40,7 @@ public class ArrowBeltItemType extends SimpleItemType implements InteractableIte
 
         namespace.getServer().getPluginManager().registerEvents(new BeltShootListener(), namespace);
 
+        this.plugin = namespace;
         playerBelts = new HashMap<>();
         playerLastShotPos = new HashMap<>();
 
@@ -105,9 +110,27 @@ public class ArrowBeltItemType extends SimpleItemType implements InteractableIte
             assert item != null;
             item.setAmount(item.getAmount() - 1);
 
-            Entity arrow = event.getProjectile();
-            event.setProjectile(player.getWorld().spawnArrow(arrow.getLocation(), arrow.getVelocity().normalize(),
-                    (float) arrow.getVelocity().length(), 0, getArrowEntity(arrowItem)));
+            assert event.getProjectile() instanceof AbstractArrow;
+            AbstractArrow oldArrow = (AbstractArrow) event.getProjectile();
+
+            AbstractArrow newArrow = player.getWorld().spawnArrow(oldArrow.getLocation(), oldArrow.getVelocity().normalize(),
+                    (float) oldArrow.getVelocity().length(), 0, getArrowEntity(arrowItem));
+
+            // Make it the same arrow
+            newArrow.setPickupStatus(oldArrow.getPickupStatus());
+            newArrow.setCritical(oldArrow.isCritical());
+            newArrow.setDamage(oldArrow.getDamage());
+            newArrow.setPierceLevel(oldArrow.getPierceLevel());
+            newArrow.setShotFromCrossbow(oldArrow.isShotFromCrossbow());
+
+            for (MetadataValue data : oldArrow.getMetadata("customArrowType")) {
+                if (data.value() instanceof CustomArrow customArrow) {
+                    newArrow.setMetadata("customArrowType", new FixedMetadataValue(plugin, customArrow));
+                    break;
+                }
+            }
+
+            event.setProjectile(newArrow);
 
             Bukkit.broadcastMessage("new projectile: " + event.getProjectile());
         }
