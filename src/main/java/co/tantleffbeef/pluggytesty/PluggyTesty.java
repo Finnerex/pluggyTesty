6ecd7b40241360b2459f09aa9b93e8c0c1f920d7
Noah Simcox <nssimcox@gmail.com>
2023-09-05 18:34:41 -0400

    added pure armor

--- src/main/java/co/tantleffbeef/pluggytesty/PluggyTesty.java
+++ src/main/java/co/tantleffbeef/pluggytesty/PluggyTesty.java
@@ -1063,7 +1063,7 @@ public final class PluggyTesty extends JavaPlugin {
         RecipeChoice.ExactChoice purifier = new RecipeChoice.ExactChoice(new ItemStack(Material.NETHERITE_UPGRADE_SMITHING_TEMPLATE));
 
 
-        RecipeChoice.ExactChoice pureLeather = new RecipeChoice.ExactChoice(new ItemStack(Material.IRON_INGOT));
+        RecipeChoice.MaterialChoice pureLeather = new RecipeChoice.MaterialChoice(Material.LEATHER);
         RecipeChoice.MaterialChoice leatherHelmet = new RecipeChoice.MaterialChoice(Material.LEATHER_HELMET);
         SmithingTransformRecipe pureLeatherHelmet = new SmithingTransformRecipe(new NamespacedKey(this, "pure_leather_helmet"), PureArmor.lH(), purifier, leatherHelmet, pureLeather);
         Bukkit.addRecipe(pureLeatherHelmet);
