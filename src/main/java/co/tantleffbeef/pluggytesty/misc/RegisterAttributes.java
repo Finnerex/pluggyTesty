8c3e4f53274bb05b208cc752c72030ad10b0a222
Noah Simcox <nssimcox@gmail.com>
2023-09-12 22:06:42 -0400

    added RegisterAttributes

--- src/main/java/co/tantleffbeef/pluggytesty/misc/RegisterAttributes.java
+++ src/main/java/co/tantleffbeef/pluggytesty/misc/RegisterAttributes.java
@@ -1,15 +1,25 @@
 package co.tantleffbeef.pluggytesty.misc;
 
+import co.tantleffbeef.pluggytesty.PluggyTesty;
 import co.tantleffbeef.pluggytesty.custom.item.armor.SimpleArmorItemType;
 import org.bukkit.Material;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
 
 public class RegisterAttributes {
-    public RegisterAttributes(String id, String name, Material material, Attribute attribute, String modName, int amount, EquipmentSlot slot){
-        new SimpleArmorItemType(this, id, false, name, material, new SimpleArmorItemType.AttributePair(attribute, new AttributeModifier(UUID.randomUUID(), modName, amount, AttributeModifier.Operation.ADD_NUMBER, slot)), new SimpleArmorItemType());
+
+    private final Plugin plugin;
+    public RegisterAttributes(@NotNull Plugin plugin, String id, String name, Material material, Attribute attribute, String modName, int amount, EquipmentSlot slot){
+        this.plugin = plugin;
+
+        new SimpleArmorItemType(plugin, id, false, name, material,
+                new SimpleArmorItemType.AttributePair(attribute, new AttributeModifier(UUID.randomUUID(), modName, amount, AttributeModifier.Operation.ADD_NUMBER, slot)),
+                new SimpleArmorItemType.AttributePair(attribute, new AttributeModifier(UUID.randomUUID(), modName, amount, AttributeModifier.Operation.ADD_NUMBER, slot)));
     }
 }
