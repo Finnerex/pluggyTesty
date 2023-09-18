1910d6a3b0ba3ca121b7ee561bbff84a61956b4b
Noah Simcox <nssimcox@gmail.com>
2023-09-18 19:13:59 -0400

    Adding Pure Stone

--- src/main/java/co/tantleffbeef/pluggytesty/custom/item/armor/PureStoneItemType.java
+++ src/main/java/co/tantleffbeef/pluggytesty/custom/item/armor/PureStoneItemType.java
@@ -1,10 +1,16 @@
 package co.tantleffbeef.pluggytesty.custom.item.armor;
 
 import co.tantleffbeef.mcplanes.custom.item.SimpleItemType;
+import org.bukkit.NamespacedKey;
 import org.bukkit.plugin.Plugin;
 
 public class PureStoneItemType extends SimpleItemType {
     public PureStoneItemType(Plugin namespace, String id, boolean customModel, String name) {
         super(namespace, id, customModel, name);
+    }
+
+    @Override
+    public NamespacedKey model(){
+        return super.model();
     }
 }
