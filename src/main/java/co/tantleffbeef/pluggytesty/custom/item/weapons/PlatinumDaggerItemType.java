97f3e2e78c45e55f23e586d89f014fa6efb81b44
Finnerex <61033009+Finnerex@users.noreply.github.com>
2023-10-23 17:23:53 -0400

    platty daggy

--- src/main/java/co/tantleffbeef/pluggytesty/custom/item/weapons/PlatinumDaggerItemType.java
+++ src/main/java/co/tantleffbeef/pluggytesty/custom/item/weapons/PlatinumDaggerItemType.java
@@ -5,12 +5,20 @@ import co.tantleffbeef.mcplanes.KeyManager;
 import co.tantleffbeef.mcplanes.ResourceManager;
 import co.tantleffbeef.mcplanes.custom.item.CustomItemType;
 import co.tantleffbeef.mcplanes.custom.item.SimpleItemType;
+import org.bukkit.Material;
+import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.UUID;
 
 public class PlatinumDaggerItemType extends SimpleItemType {
 
@@ -18,11 +26,21 @@ public class PlatinumDaggerItemType extends SimpleItemType {
     private final ResourceManager resourceManager;
 
     public PlatinumDaggerItemType(Plugin namespace, String id, boolean customModel, String name, KeyManager<CustomNbtKey> keyManager, ResourceManager resourceManager) {
-        super(namespace, id, customModel, name);
+        super(namespace, id, customModel, name, Material.IRON_SWORD);
         this.keyManager = keyManager;
         this.resourceManager = resourceManager;
 
         namespace.getServer().getPluginManager().registerEvents(new PlatinumDaggerAttackListener(), namespace);
+    }
+
+    @Override
+    public void modifyItemMeta(@NotNull ItemMeta meta) {
+        super.modifyItemMeta(meta);
+        meta.setUnbreakable(true);
+        meta.addAttributeModifier(Attribute.GENERIC_ATTACK_SPEED,
+                new AttributeModifier(UUID.randomUUID(), "pluggyTesty", 6, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.HAND));
+        meta.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE,
+                new AttributeModifier(UUID.randomUUID(), "pluggyTesty", 4, AttributeModifier.Operation.ADD_NUMBER, EquipmentSlot.HAND));
     }
 
     private class PlatinumDaggerAttackListener implements Listener {
